d1cfaa54e3a446f5f0a2fefa70c9aa44ccbccb9b
Matthieu Nourry <matthieu.nourry@hotmail.fr>
2018-10-29 19:55:43 +0100

    source enclos pour lou <3

---
+++ src/Enclos/Voliere.java
@@ -0,0 +1,10 @@
+package Enclos;
+
+public class Voliere extends Enclos{
+
+	public Voliere(String nom, int superficie, int animauxMax, int nbAnimaux, String propreté) {
+		super(nom, superficie, animauxMax, nbAnimaux, propreté);
+		// TODO Auto-generated constructor stub
+	}
+
+}
