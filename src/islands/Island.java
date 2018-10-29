06881b6b11a74d66d43cf9ac6169cc191e4b1bb2
Byrnes <lou.byrnes@etu.univ-amu.fr>
2018-10-29 19:54:23 +0100

    Creations sousclasses island

--- src/islands/Island.java
+++ src/islands/Island.java
@@ -4,10 +4,28 @@ import creatures.Dinosaur;
 
 import java.util.ArrayList;
 
-public class Island {
-    public String nom;
-    public int superficie;
-    public int animauxMax, nbAnimaux;
-    public String propreté; //Mauvais, Bon, Correct
-    public ArrayList<Dinosaur> dinosaurs;
+public abstract class Island<T>{
+    private String nom;
+    private int superficie;
+    private int animauxMax, nbAnimaux;
+    private String propreté; //Mauvais, Bon, Correct
+    private ArrayList<T> creatures;
+
+    public void afficherCaracs(){
+        this.toString();
+        for()
+    }
+
+    @Override
+    public String toString() {
+        return "Island{" +
+                "nom='" + nom + '\'' +
+                ", superficie=" + superficie +
+                ", animauxMax=" + animauxMax +
+                ", nbAnimaux=" + nbAnimaux +
+                ", propreté='" + propreté + '\'' +
+                ", creatures=" + creatures +
+                '}';
+    }
+
 }
